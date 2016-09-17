da8bc95216e7018ca47b5911d92617d7126c3e76
Y-Chi Lu <luyege+github@gmail.com>
2016-09-16 22:19:52 -0500

    'v1'

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -4,16 +4,37 @@
 
 
     angular.module('LunchCheck',[])
-    .controller('LunchController', LunchController);
+    .controller('LunchCheckController', LunchCheckController);
     //.controller('LunchController',function($scope){        $scope.name="yakov";    });
 
-    LunchController.$inject=['$scope'];
-
-    function LunchController($scope){
+    LunchCheckController.$inject=['$scope','$log'];
+   // LunchController.$inject=['$log'];
+    function LunchCheckController($scope){
      $scope.name= "Yaakov1abc"; 
      
+     $scope.state="";
+
      $scope.sayMsg= function(){
-   return "test function";
+   return "list comma separated dishes you usually have for lunch";
+     };
+
+     $scope.displayState=function()
+     {
+     // console.log( $scope.tbinput );
+      //var tArray=[];
+      if( $scope.tbinput == undefined || $scope.tbinput=="")
+      {
+     $scope.state ="Please enter data first";
+     }
+    else{ 
+    var tArray=$scope.tbinput.split(",");
+     if (tArray.length<=3 )
+     {
+           $scope.state ="Enjoy";
+     }
+     else{  $scope.state ="Too much!";}
+    }
+       
      };
     }
 
