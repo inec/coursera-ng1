aa00293b0e3c455bac3e990680b7fd35d32fddbc
Y-Chi Lu <luyege+github@gmail.com>
2016-09-19 23:58:31 -0500

    mid

---
+++ mod2_solution/app.js
@@ -0,0 +1,77 @@
+
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBroughtShoppingController', AlreadyBroughtShoppingController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService) {
+  var itemAdder = this;
+
+  itemAdder.itemName = "";
+  itemAdder.itemQuantity = "";
+
+  itemAdder.addItem = function () {
+    ShoppingListCheckOffService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+  }
+}
+
+
+AlreadyBroughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBroughtShoppingController(ShoppingListCheckOffService) {
+  var showList = this;
+
+  showList.items = ShoppingListCheckOffService.getItems();
+
+  showList.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var items = [
+  {
+    name: "Milk",
+    quantity: "2"
+  },
+  {
+    name: "Donuts",
+    quantity: "200"
+  },
+  {
+    name: "Cookies",
+    quantity: "300"
+  },
+  {
+    name: "Chocolate",
+    quantity: "5"
+  }
+];
+
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    items.push(item);
+  };
+
+  service.removeItem = function (itemIdex) {
+    console.log(itemIdex);
+    items.splice(itemIdex, 1);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+}
+
+})();
